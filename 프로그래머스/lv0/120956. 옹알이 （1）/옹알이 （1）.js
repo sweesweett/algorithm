7c42f851986c3a4e708115b54be21f24937ed910
Yunjin Lee <sweesweet@naver.com>
2022-11-07 22:56:37 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.js
@@ -1,6 +1,5 @@
 function solution(babbling) {
    let str= babbling.join(',')
-
    str=str.replaceAll(/(aya|ye|woo|ma)/g,'')
     let num=str.split(',').filter(el=>el!=='').length
     return babbling.length - num
