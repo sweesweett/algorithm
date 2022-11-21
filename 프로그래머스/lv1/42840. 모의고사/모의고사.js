1d41648e192e50e90a7cbbaa51a223ff0181b356
Yunjin Lee <sweesweet@naver.com>
2022-11-21 12:56:02 +0900

    [level 1] Title: 모의고사, Time: 3.32 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,51 @@
+function solution(answers) {
+   let answer = [];
+    let aa=[1,2,3,4,5]
+    let bb=[2,1,2,3,2,4,2,5]
+    let cc=[3,3,1,1,2,2,4,4,5,5]
+    let obj={1:0,2:0,3:0}
+    
+    
+    for(let i =0 ; i<answers.length;i++){
+        if(aa[i]===undefined){
+            if(aa[i%5]===answers[i]){
+                obj[1]+=1
+            }
+        }else{
+            if(aa[i]===answers[i]){
+                obj[1]+=1
+            }
+            
+            
+        }
+         if(bb[i]===undefined){
+            if(bb[i%8]===answers[i]){
+                obj[2]+=1
+            }
+        }else{
+            if(bb[i]===answers[i]){
+                obj[2]+=1
+            }
+            
+            
+        }
+         if(cc[i]===undefined){
+            if(cc[i%10]===answers[i]){
+                obj[3]+=1
+            }
+        }else{
+            if(cc[i]===answers[i]){
+                obj[3]+=1
+            }
+            
+            
+        }
+    }
+    let max = Math.max(...Object.values(obj))
+    for (let key in obj){
+        if(obj[key]===max){
+            answer.push(Number(key))
+        }
+    }
+    return answer
+}
