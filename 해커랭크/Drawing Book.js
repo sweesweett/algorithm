cdb36a36908761c59151374fd38e518d4d1bb37d
Yunjin Lee <sweesweet@naver.com>
2022-11-24 00:44:08 +0900

    Create Drawing Book.js

---
+++ 해커랭크/Drawing Book.js
@@ -0,0 +1,63 @@
+//https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+   let dd=Math.floor(n/2)+1
+   let pp = Math.floor(p/2)+1
+   if(dd==pp){
+       return 0
+   }
+   let arr=[pp-1,dd-pp]
+   if(arr[0]>=arr[1]){
+       return arr[1]
+   }else{
+       return arr[0]
+   }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = parseInt(readLine().trim(), 10);
+
+    const result = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
