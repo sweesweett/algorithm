15c261d1752d4a2a343e463fa7502efa53bdfd4e
Yunjin Lee <sweesweet@naver.com>
2022-11-25 16:24:40 +0900

    [unrated] Title: 과일 장수, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,16 +1,13 @@
 function solution(k, m, score) {
    let arr=[...score]
     let length=0
-    let sum=[]
     let answer=0
      arr.sort((a,b)=>a-b)
     for(let i=arr.length-1;i>=0;i--){
-        sum.push(arr[i])
         length++
         if(length===m){
-          answer+=sum[sum.length-1]*sum.length
+          answer+=arr[i]*length
           length=0
-          sum=[]
       }
         
     }
