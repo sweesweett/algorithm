e0a61902f16eeca353ad7f77f86816aaf1432b6d
Yunjin Lee <sweesweet@naver.com>
2022-11-29 02:09:15 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.17 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/72410. 신규 아이디 추천/README.md
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/README.md
@@ -1,10 +1,10 @@
 # [level 1] 신규 아이디 추천 - 72410 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72410#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.18 ms
+메모리: 33.4 MB, 시간: 0.17 ms
 
 ### 구분
 
--- 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,10 +1,10 @@
 function solution(new_id) {
     let id=new_id.toLowerCase()
-   id=id.replaceAll(/[^0-9a-z-_.]/g,'')
+   id=id.replace(/[^0-9a-z-_.]/g,'')
        
-    id=id.replaceAll(/[.]{2,}/g,'.')
+    id=id.replace(/[.]{2,}/g,'.')
   // return id
-    id=id.replaceAll(/^[.]|[.]$/g,'')
+    id=id.replace(/^[.]|[.]$/g,'')
 
     
     if(id.length===0){
