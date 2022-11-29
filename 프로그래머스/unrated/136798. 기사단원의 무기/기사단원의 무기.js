57b418238b2f72bb453b07f65505ab38a3bc7ab5
Yunjin Lee <sweesweet@naver.com>
2022-11-29 13:02:56 +0900

    [unrated] Title: 기사단원의 무기, Time: 5.58 ms, Memory: 37.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.js
@@ -0,0 +1,28 @@
+function solution(number, limit, power) {
+    var answer = [1]
+    for(let i =2;i<=number;i++){
+        answer.push(dd(i))
+    }
+    
+    console.log(answer)
+    return answer.reduce((a,b)=>{
+        if(b>limit){
+            return a+power
+        }else{
+            return a+b
+        }
+        
+    },0)
+}
+function dd(num){
+    let answer=0
+    for(let i =1;i<=num**0.5;i++){
+        if(i===num**0.5){
+            answer+=1
+        }
+        else if(num%i===0){
+            answer+=2
+        }
+    }
+    return answer
+}
