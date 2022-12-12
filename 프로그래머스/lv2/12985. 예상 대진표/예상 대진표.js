c043e939f9db06ef61ca20bf27356e91c709df35
Yunjin Lee <sweesweet@naver.com>
2022-12-12 11:56:47 +0900

    [level 2] Title: 예상 대진표, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.js
@@ -0,0 +1,17 @@
+function solution(n,a,b)
+{
+let aa=a
+let bb=b
+let i =0
+while(true){
+    if(aa===bb){
+        return i
+    }
+    aa=Math.ceil(aa/2)
+    bb=Math.ceil(bb/2)
+    i++
+    
+    
+}
+
+}
