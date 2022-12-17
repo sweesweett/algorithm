cea44d96351a7b9619e8a7c23586cc28bee8dac5
Yunjin Lee <sweesweet@naver.com>
2022-12-18 01:36:09 +0900

    Update n＾2 배열 자르기.js

--- 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.js
@@ -1,23 +1,17 @@
 function solution(n, left, right) {
     let arr=[]
-    
     for(let i =left;i<=right;i++){
-       
-        if(i<n){
-         arr.push(i+1)
-            
-        }else{
-        if((i+1)%n===0){
+            let rest =(i+1)%n
+            let dd= Math.floor((i+1)/n)
+        if(rest===0){
             arr.push(n)
         }
-        else if(Math.floor((i+1)/n)>=(i+1)%n){
-            arr.push(Math.floor((i+1)/n)+1)
+        else if(dd>=rest){
+            arr.push(dd+1)
         }else{
            
-                arr.push((i+1)%n)  
-          
-                  
-    }
+                arr.push(rest)          
+  
         }
     }
     return arr
