e8e85365b53070d2abb5d366b08b7caf798f04e0
Yunjin Lee <sweesweet@naver.com>
2023-03-28 17:20:58 +0900

    Update 카드 뭉치.js

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,23 +1,28 @@
 function solution(cards1, cards2, goal) {
 const isFirst=[true,true]
 for(let i =goal.length-1;i>=0;i--){
-    if(cards1.length&&cards1[cards1.length-1]===goal[i]){
+    if(cards1[cards1.length-1]===goal[i]){
         cards1.pop()
         isFirst[0]=false
-    }else if (cards2.length&&cards2[cards2.length-1]===goal[i]){
+    }else if (cards2[cards2.length-1]===goal[i]){
         cards2.pop()
         isFirst[1]=false
     }
     else if(isFirst[0]&&goal.indexOf(cards1[cards1.length-1])===-1){
         cards1.pop()
+        i++
     }
      else if(isFirst[1]&&goal.indexOf(cards2[cards2.length-1])===-1){
         cards2.pop()
+         i++
     }
     else{
         return 'No'
     }
 }
 
+if(cards1.length+cards2.length){
+    return 'No'
+}
 return 'Yes'
 }
