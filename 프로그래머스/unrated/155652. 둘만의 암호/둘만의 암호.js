cab49da8fa7c9a24c8b2b18595e9f1ebfcf72925
Yunjin Lee <sweesweet@naver.com>
2023-03-31 01:09:41 +0900

    [unrated] Title: 둘만의 암호, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/155652. 둘만의 암호/둘만의 암호.js
@@ -0,0 +1,14 @@
+function solution(s, skip, index) {
+    let newWords=''
+    let alpha='abcdefghijklmnopqrstuvwxyz'
+for(let i =0;i<skip.length;i++){
+           alpha=alpha.replace(skip[i],'') 
+
+}
+for(let i =0; i<s.length;i++){
+        const idx = (alpha.indexOf(s[i])+index)%(26-skip.length)
+
+     newWords+=alpha[idx]
+}
+    return newWords
+}
