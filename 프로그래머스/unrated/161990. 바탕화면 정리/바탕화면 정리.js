1a15314cd1d473dbce6f2fa4ddd0517d1e81f891
Yunjin Lee <sweesweet@naver.com>
2023-04-02 22:41:16 +0900

    [unrated] Title: 바탕화면 정리, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
+++ 프로그래머스/unrated/161990. 바탕화면 정리/바탕화면 정리.js
@@ -1,19 +1,29 @@
 function solution(wallpaper) {
-    var answer = [];
-    // let max=[-1,-1]
-    // let min=[wallpaper.length,wallpaper[0].length]
-    const x=[]
-    const y=[]
+    let max=[-1,-1]
+    let min=[wallpaper.length,wallpaper[0].length]
     wallpaper.forEach((el,idx)=>{
         for(let i =0;i<el.length;i++){
             if(wallpaper[idx][i]==='#'){
-             x.push(idx)
-            y.push(i)
-                
-            
-            
-        }}
-        
+                if(max[1]<i){
+                    max[1]=i
+                }
+                if(max[0]<idx){
+                    max[0]=idx
+                }
+                if(min[1]>i){
+                    min[1]=i
+                }
+                if(min[0]>idx){
+                    min[0]=idx
+                }
+
+
+                }
+
+
+
+        }
+
     })
-    return [Math.min(...x),Math.min(...y),Math.max(...x)+1,Math.max(...y)+1]
+    return [...min,max[0]+1,max[1]+1]
 }
