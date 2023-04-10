f7d0c99afd61d62956c729af510738b1ebc6a0fb
Yunjin Lee <sweesweet@naver.com>
2023-04-10 23:42:13 +0900

    Time: 48 ms (99.23%), Space: 42.6 MB (30.95%) - LeetHub

--- Leetcode/Easy/1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
+++ Leetcode/Easy/1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -11,6 +11,9 @@ var areAlmostEqual = function(s1, s2) {
     for(let i =0;i<s1.length;i++){
         if(s1[i]!==s2[i]){
             dd.push(i)
+            if(dd.length>2){
+                return false
+            }
         }
 
     }
