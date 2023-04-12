240ffe714d67d22ab20d358016328c1d827ae2b8
Yunjin Lee <sweesweet@naver.com>
2023-04-12 20:02:20 +0900

    Time: 94 ms (52.40%), Space: 49.8 MB (26.85%) - LeetHub

--- Leetcode/Easy/0344-reverse-string/0344-reverse-string.js
+++ Leetcode/Easy/0344-reverse-string/0344-reverse-string.js
@@ -3,5 +3,12 @@
  * @return {void} Do not return anything, modify s in-place instead.
  */
 var reverseString = function(s) {
-    return s.reverse()
+       let length=s.length-1
+    for(let i=0;i<s.length/2;i++){
+    
+        let tmp=s[i]
+        s[i]=s[length-i]
+        s[length-i]=tmp
+        
+    }
 };
