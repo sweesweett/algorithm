479475dce5e6eb5519ddda289a1b27a6157ea67b
Yunjin Lee <sweesweet@naver.com>
2023-04-17 18:39:50 +0900

    Time: 62 ms (78.52%), Space: 44 MB (49.58%) - LeetHub

--- Leetcode/Easy/0202-happy-number/0202-happy-number.js
+++ Leetcode/Easy/0202-happy-number/0202-happy-number.js
@@ -6,11 +6,13 @@ var isHappy = function(n) {
     let map=new Map();
     let num=n
     while(true){
-        let arr=[...String(num)].map(Number)
         let sum=0
-        for(let val of arr){
-            sum+= val**2
+        while(num>1){
+            let rest= num%10
+            sum=sum+rest**2
+            num=(num-rest)/10
         }
+        sum+=(num%10)**2
         if(map.has(sum)){
             return false
         }
