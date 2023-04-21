24ed18dd41ef7da6e82f2c60f49abdb1d80e4ebb
Yunjin Lee <sweesweet@naver.com>
2023-04-22 01:26:53 +0900

    Time: 92 ms (61.43%), Space: 53.7 MB (19.88%) - LeetHub

--- Leetcode/Easy/0896-monotonic-array/0896-monotonic-array.js
+++ Leetcode/Easy/0896-monotonic-array/0896-monotonic-array.js
@@ -8,18 +8,13 @@ var isMonotonic = function(nums) {
         let pp=nums[i+1]-nums[i]
         if(pp<0){
             tt[0]=false
-            break
+           
         }
-        
-    }
-    for(let i =0;i<nums.length-1;i++){
-        let pp=nums[i+1]-nums[i]
         if(pp>0){
             tt[1]=false
-            break
         }
-     
         
     }
+ 
     return tt[0]||tt[1]
 };
