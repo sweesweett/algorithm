cc2f4c0fdbf70d5e77b275af9ec1219895f86640
Yunjin Lee <sweesweet@naver.com>
2023-04-26 22:32:12 +0900

    Time: 94 ms (81.26%), Space: 44.9 MB (85.11%) - LeetHub

--- Leetcode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ Leetcode/Easy/0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -3,20 +3,12 @@
  * @return {number}
  */
 var firstUniqChar = function(s) {
-    let obj={}
+  
     for(let i=0;i<s.length;i++){
-        if(obj[s[i]]!==undefined){
-            obj[s[i]].push(i)
-
-        }else{
-            obj[s[i]]=[i]
-
-        }
+       if(s.indexOf(s[i])===s.lastIndexOf(s[i])){
+           return i
+       }
     }
-    let lengthOne=Object.values(obj).filter(el=>el.length===1)
-    if(lengthOne.length===0){
-        return -1
-    }
-    return Math.min(...lengthOne.flat())
+    return -1
     
 };
