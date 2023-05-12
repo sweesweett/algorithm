5c6b8a772c85f4a2161deb5c1ef6ce82c603172a
Yunjin Lee <sweesweet@naver.com>
2023-05-12 12:52:40 +0900

    Time: 100 ms (43.81%), Space: 48.8 MB (17.00%) - LeetHub

--- Leetcode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ Leetcode/Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -5,14 +5,15 @@
  */
 var maxVowels = function(s, k) {
     let set=new Set(['a','e','i','o','u'])
-    let arr=[...s].map(el=>set.has(el)?1:0)
-    let sum=arr.slice(0,k).reduce((a,b)=>a+b,0)
+    let arr=[...s]
+    let sum=arr.slice(0,k).reduce((a,b)=>a+Number(set.has(b)),0)
     let max=sum
+    
     if(sum===k){
         return k
     }
     for(let i =k;i<arr.length;i++){
-        let dd= sum+arr[i]-arr[i-k]
+        let dd= sum+=Number(set.has(arr[i]))-Number(set.has(arr[i-k]))
         if(dd===k){
             return k
         }else{
