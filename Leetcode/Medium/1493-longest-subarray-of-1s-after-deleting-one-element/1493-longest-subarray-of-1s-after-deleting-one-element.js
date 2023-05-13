9c63253643462f04a6417631b8a201c1f1b16654
Yunjin Lee <sweesweet@naver.com>
2023-05-13 22:05:10 +0900

    Time: 62 ms (87.89%), Space: 47.5 MB (20.42%) - LeetHub

--- Leetcode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
+++ Leetcode/Medium/1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.js
@@ -8,22 +8,20 @@ var longestSubarray = function(nums) {
     }
     let count=0
     let prev=0
-    let max=[]
+    let max=0
     for(let val of nums){
         if(val===1){
             count++
         }else{
-            max.push(prev+count)
+                max=Math.max(max,prev+count)
                 prev=count
                 count=0
          
         }
     }
-    if(count){
-        max.push(count+prev)
-    }
-    if(max[0]===nums.length){
+    max=Math.max(prev+count,max)
+    if(max===nums.length){
         return nums.length-1
     }
-    return max.length===0?0:Math.max(...max)
+    return max
 };
