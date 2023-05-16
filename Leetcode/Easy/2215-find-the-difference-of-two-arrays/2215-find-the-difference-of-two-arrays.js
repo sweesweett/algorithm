4ff1f442822371641320de4b485091096191c4c7
Yunjin Lee <sweesweet@naver.com>
2023-05-17 00:01:01 +0900

    Time: 83 ms (92.42%), Space: 48.9 MB (52.90%) - LeetHub

--- Leetcode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
+++ Leetcode/Easy/2215-find-the-difference-of-two-arrays/2215-find-the-difference-of-two-arrays.js
@@ -7,14 +7,13 @@ var findDifference = function(nums1, nums2) {
     let aSet= new Set(nums1)
     let bSet= new Set(nums2)
     const findD=(set,comp)=>{
-        let compSet= new Set(comp)
         let newArr=[]
         for(let val of set){
-            if(!compSet.has(val)){
+            if(!comp.has(val)){
                 newArr.push(val)
             }
         }
         return newArr
     }
-    return[findD(aSet,nums2),findD(bSet,nums1)]
+    return[findD(aSet,bSet),findD(bSet,aSet)]
 };
