f531d0b575d6a06e7147ddc9dc9c18ac5ce3644b
Yunjin Lee <sweesweet@naver.com>
2023-05-26 23:37:41 +0900

    Time: 61 ms (48.98%), Space: 42.1 MB (51.02%) - LeetHub

--- Leetcode/Medium/0388-longest-absolute-file-path/0388-longest-absolute-file-path.js
+++ Leetcode/Medium/0388-longest-absolute-file-path/0388-longest-absolute-file-path.js
@@ -14,28 +14,28 @@ var lengthLongestPath = function(input) {
     for(let i =0;i<arr.length;i++){
         if(!arr[i].startsWith('/')&&num===0){
             stack.push(arr[i])
-            length+=arr[i].length+1
+            length+=arr[i].length
             num++
         }
         else if(arr[i].slice(0,num)==='/'.repeat(num)&&arr.at(num)!=='/'&&num>0){
-            let val=arr[i].slice(num)
+            let val=arr[i].slice(num-1)
             if(i===arr.length-1&&!arr[i].includes('.')){
                 return maxLength
             }
             stack.push(val)
-            length+=val.length+1
+            length+=val.length
             num++
           
         }else{
             if(stack.at(-1).includes('.')){
                 maxLength=Math.max(maxLength,length)
             }
-             length-=stack.pop().length+1
+             length-=stack.pop().length
              i--
              num-- 
         }
 
     }
-    return Math.max(maxLength-1,length-1)
+    return Math.max(maxLength,length)
     
 };
