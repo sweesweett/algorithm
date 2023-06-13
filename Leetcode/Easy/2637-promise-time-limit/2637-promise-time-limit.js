aeedfe45a979de2dc0a05be830836af3ebd51e45
Yunjin Lee <sweesweet@naver.com>
2023-06-13 23:56:58 +0900

    Time: 64 ms (42.25%), Space: 42.2 MB (20.05%) - LeetHub

---
+++ Leetcode/Easy/2637-promise-time-limit/2637-promise-time-limit.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+	return async function(...args) {
+        let origin=fn(...args)
+        let dd= new Promise((_,reject)=>setTimeout(()=>reject('Time Limit Exceeded'),t))
+        return Promise.race([origin,dd])
+      
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
