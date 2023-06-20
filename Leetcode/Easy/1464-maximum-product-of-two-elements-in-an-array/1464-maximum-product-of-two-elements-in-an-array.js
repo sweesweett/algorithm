ab524b223d689d4632e15de8c0ca65ec9cf3697d
Yunjin Lee <sweesweet@naver.com>
2023-06-20 19:43:54 +0900

    Time: 89 ms (5.46%), Space: 45.8 MB (5.71%) - LeetHub

--- Leetcode/Easy/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
+++ Leetcode/Easy/1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.js
@@ -4,15 +4,9 @@
  */
 var maxProduct = function(nums) {
     let pr = new MaxPriorityQueue()
-    for(let i=0;i<nums.length-1;i++){
-        for(let j=i+1;j<nums.length;j++){
-            let val= (nums[i]-1)*(nums[j]-1)
-            if(!pr.front()||pr.front().priority<val){
-                pr.enqueue(val)
-            }
-            
-        }
+    for(let i=0;i<nums.length;i++){
+        pr.enqueue(nums[i])
     }
 
-    return pr.front().priority
+    return (pr.dequeue().priority-1)*(pr.dequeue().priority-1)
 };
