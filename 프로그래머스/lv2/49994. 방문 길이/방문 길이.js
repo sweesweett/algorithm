83002fa23d770766ee7eea54329b5df6ec7f554c
Yunjin Lee <sweesweet@naver.com>
2023-06-28 14:17:41 +0900

    [level 2] Title: 방문 길이, Time: 1.53 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/49994. 방문 길이/방문 길이.js
@@ -0,0 +1,51 @@
+function solution(dirs) {
+    let dir=[5,5]
+    let set = new Set()
+    let dd=[]
+    for(let i =0;i<=10;i++){
+        let arr=[]
+        for(let j=0;j<=10;j++){
+            arr.push(10)
+        }
+        dd.push(arr)
+    }
+    let count=0
+    
+    for(let i =0;i<dirs.length;i++){
+        let dirr=[...dir]
+        if(dirs[i]==='L'){
+            getdir(dir,-1,0)
+        }else if(dirs[i]==='U'){
+            
+            getdir(dir,0,1)
+        }else if(dirs[i]==='D'){
+            getdir(dir,0,-1)
+        }else{
+            getdir(dir,1,0)
+        }
+        if(dir.join('')===dirr.join('')){
+            continue
+        }
+        if(!set.has(dir.join('')+dirr.join(''))&&!set.has(dirr.join('')+dir.join(''))){
+            set.add(dir.join('')+dirr.join(''))
+            set.add(dirr.join('')+dir.join(''))
+            count++
+        }
+
+        
+    }
+    return count
+}
+function getdir(dir,x,y){
+    dir[0]+=x
+    dir[1]+=y
+    if(dir[0]>10){
+        dir[0]=10
+    }else if(dir[0]<0){
+        dir[0]=0
+    }else if(dir[1]>10){
+        dir[1]=10
+    }else if(dir[1]<0){
+        dir[1]=0
+    }
+}
