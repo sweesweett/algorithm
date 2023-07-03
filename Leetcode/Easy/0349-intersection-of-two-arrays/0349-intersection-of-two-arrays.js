dd91d0a61fd2efe7edd279fbc7b1de51f6dc9507
Yunjin Lee <sweesweet@naver.com>
2023-07-03 21:51:22 +0900

    Time: 65 ms (45.39%), Space: 43.6 MB (39.25%) - LeetHub

--- Leetcode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
+++ Leetcode/Easy/0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -4,15 +4,14 @@
  * @return {number[]}
  */
 var intersection = function(nums1, nums2) {
-    let set = new Set()
-    let arr=[]
-    nums1.forEach(el=>set.add(el))
+    let set = new Set(nums1)
+    let set2= new Set()
     nums2.forEach(el=>{
         if(set.has(el)){
-            arr.push(el)
-            set.delete(el)
+            set2.add(el)
         }
 
     })
-    return arr
+
+    return [...set2]
 };
