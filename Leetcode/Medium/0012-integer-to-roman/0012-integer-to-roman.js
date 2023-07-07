a452131a24a8053463f6ffd658d7db34dcf4b43b
Yunjin Lee <sweesweet@naver.com>
2023-07-07 22:03:08 +0900

    Time: 145 ms (19.37%), Space: 51.1 MB (9.99%) - LeetHub

--- Leetcode/Medium/0012-integer-to-roman/0012-integer-to-roman.js
+++ Leetcode/Medium/0012-integer-to-roman/0012-integer-to-roman.js
@@ -5,24 +5,18 @@
 var intToRoman = function(num) {
 
 let map = new Map([[1,'I'],[5,'V'],[10,'X'],[50,'L'],[100,'C'],[500,'D'],[1000,'M'],[4,"IV"],[9,'IX'],[40,"XL"],[90,"XC"],[400,'CD'],[900,'CM']])
-let arr=[]
-let max= String(num).length-1
-while(max>=0){
-arr.push(Math.trunc(num/10**max)*10**max)
-num%=10**max
-max--
-
-}
+let arr=String(num).split('').map(Number)
 return arr.map((num,idx)=>{
-if(map.has(num)){
-  return map.get(num)
+ let tenten=10**(arr.length-1-idx)    
+if(map.has(num*tenten)){
+  return map.get(num*tenten)
 }else{
-  let trunc=Math.trunc(num/10**(arr.length-1-idx))
-  if(trunc>5){
-    return map.get(10**(arr.length-1-idx)*5)+`${map.get(10**(arr.length-1-idx))}`.repeat(trunc-5)
+   
+  if(num>5){
+    return map.get(tenten*5)+`${map.get(tenten)}`.repeat(num-5)
   }else{
 
-    return `${map.get(10**(arr.length-1-idx))}`.repeat(trunc)
+    return `${map.get(tenten)}`.repeat(num)
   }
 
 }
