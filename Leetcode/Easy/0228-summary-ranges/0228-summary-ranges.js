ecd9f941508d7b07c8519ca569761652ad020ed1
Yunjin Lee <sweesweet@naver.com>
2023-09-23 23:48:34 +0900

    Time: 46 ms (81.09%), Space: 42.2 MB (28.53%) - LeetHub

--- Leetcode/Easy/0228-summary-ranges/0228-summary-ranges.js
+++ Leetcode/Easy/0228-summary-ranges/0228-summary-ranges.js
@@ -6,19 +6,14 @@ var summaryRanges = function(nums) {
     if(nums.length===0){
         return []
     }
-   let start=0,prev=0
+   let start=0
    let ans=[]
    for(let i=1;i<=nums.length;i++){
-       if(nums[i]-nums[prev]!==1||nums[i]===undefined){
-           if(prev-start>0){
-               ans.push(nums[start]+'->'+nums[prev])
-           }else{
-               ans.push(nums[start]+'')
-           }
-           start=i
-       }
-        prev=i
-       
+       if(nums[i]-nums[i-1]===1)continue
+       let str= i-start>1?`${nums[start]}->${+nums[i-1]}`:`${nums[start]}`
+       ans.push(str)
+       start=i
+   
    }
 
    return ans
