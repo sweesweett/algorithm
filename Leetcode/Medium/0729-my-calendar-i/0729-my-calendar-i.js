e36880856bbb7f4446a69946b96f27b390c7e03d
Yunjin Lee <sweesweet@naver.com>
2023-10-05 14:55:15 +0900

    Time: 181 ms (23.26%), Space: 49.8 MB (97.67%) - LeetHub

--- Leetcode/Medium/0729-my-calendar-i/0729-my-calendar-i.js
+++ Leetcode/Medium/0729-my-calendar-i/0729-my-calendar-i.js
@@ -1,7 +1,6 @@
 
 var MyCalendar = function() {
     this.set= new Set()
-
 };
 
 /** 
@@ -10,14 +9,61 @@ var MyCalendar = function() {
  * @return {boolean}
  */
 MyCalendar.prototype.book = function(start, end) {
- 
     for(let val of this.set){
         let [l,r]= val
-        if (end > l && start <r) return false;
+        if(start<l&&end<=l){
+            continue
+        }
+        if(r<=start){
+            continue
+        }
+        if(l===start){
+            return false
+        }
+        if(start>l){
+            if(!binarySearch(l,r,start)){
+                return false
+            }
+        }
+        else if(end>r){
+            return false
+        }
+        else{
+           if(!binarySearch(l,r,end)){
+                return false
+            }
+        }
+   
+         
     }
     this.set.add([start,end])
     return true
 };
+
+
+function binarySearch(l,r,compare){
+    while(l<r){
+            let mid= Math.floor((l+r)/2)
+            if(mid===compare){
+                return false
+            }
+            if(mid<compare){
+                l=mid+1
+            }else{
+                r=mid-1
+            }
+        }
+            if(l===compare){
+            return false
+        }
+
+    return true
+
+
+}
+
+
+
 
 /** 
  * Your MyCalendar object will be instantiated and called as such:
