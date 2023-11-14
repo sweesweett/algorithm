3160c11fb823b75c65192a6e18fef50a2b089490
Yunjin Lee <sweesweet@naver.com>
2023-11-14 22:21:32 +0900

    [level 3] Title: 단어 변환, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.js
@@ -0,0 +1,37 @@
+function solution(begin, target, words) {
+    let min=words.length
+    let set= new Set(words)
+    if(!set.has(target)){
+        return 0
+    }
+  const dfs=(word,total)=>{
+      if(total>min){
+          return;
+      }
+      if(word===target){
+          min= Math.min(min,total)
+          return;
+      }
+      if(total===words.length){
+          return;
+      }
+      for(let i=0;i<words.length;i++){
+          if(words[i]===word){
+              continue
+          }
+          let count=0
+          for(let j=0;j<words[i].length;j++){
+              if(word[j]!==words[i][j]){
+                  count++
+              }
+          }
+          if(count===1){
+              dfs(words[i],total+1)
+          }
+      }
+
+  }
+  dfs(begin,0)
+ 
+ return min
+}
