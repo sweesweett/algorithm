b72d7576fe4ddd8a083d583c83ff6f65eadff10b
Yunjin Lee <sweesweet@naver.com>
2023-11-19 00:26:47 +0900

    Time: 61 ms (94.82%), Space: 50 MB (32.72%) - LeetHub

--- Leetcode/Easy/0338-counting-bits/0338-counting-bits.js
+++ Leetcode/Easy/0338-counting-bits/0338-counting-bits.js
@@ -2,23 +2,30 @@
  * @param {number} n
  * @return {number[]}
  */
+//dp 다시 이해하고 풀어보는중!
 var countBits = function(n) {
     if(n===0){
         return [0]
     }
-    let arr=[0,1]
-    for(let i=2;i<=n;i++){
-        let dd=i
-        let count=0
-       while(dd>1){
-           count+=dd%2
+    if(n===1){
+        return [0,1]
+    }
 
-           dd=(dd-dd%2)/2
-       }
-       if(dd===1){
-           count+=1
-       }
-       arr.push(count)
+    let arr=[0,1,1]
+    for(let i=3;i<=n;i++){
+        let num=i
+        let val=0
+        while(num>=1){
+            if(arr[num]){
+                val+=arr[num]
+                break
+            }
+            let div=Math.floor(num/2)
+            let rest=num%2
+            val+=arr[rest]
+            num=div
+        }
+        arr.push(val)
     }
     return arr
 };
