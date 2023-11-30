dc212efc087072be6bb6fbc675765d799ca49d60
Yunjin Lee <sweesweet@naver.com>
2023-11-30 21:00:07 +0900

    Time: 85 ms (50.76%), Space: 49.9 MB (64.92%) - LeetHub

--- Leetcode/Easy/0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
+++ Leetcode/Easy/0617-merge-two-binary-trees/0617-merge-two-binary-trees.js
@@ -12,19 +12,26 @@
  * @return {TreeNode}
  */
 var mergeTrees = function(root1, root2) { 
- const dfs=(node1,node2)=>{
-  if(node1===null){
-     return node2
-   }
-   if(node2===null){
-       return node1
-   }
-    node1.val=node1.val+node2.val
-   node1.left=dfs(node1.left,node2.left)
-   node1.right=dfs(node1.right,node2.right)
-   return node1
+    if(!root1) return root2
+    if(!root2) return root1
+    let ans= root1
+    let queue=[[ans,root2]]
+    while(queue.length){
+        let [r1,r2]= queue.shift()
+        r1.val+=r2.val
+        if(r1.left&&r2.left){
+            queue.push([r1.left,r2.left])
+        }else if(!r1.left){
+            r1.left=r2.left
+        }
+        if(r1.right&&r2.right){
+            queue.push([r1.right,r2.right])
+        }else if(!r1.right){
+            r1.right=r2.right
+        }
 
- }
- 
-  return dfs(root1,root2)
-};
+
+    }
+    return ans
+
+}
