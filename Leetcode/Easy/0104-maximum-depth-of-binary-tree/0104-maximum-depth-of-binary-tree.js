5e516528f5faeb89ac0bdb2e8f8cb2b0a55ab7ee
Yunjin Lee <sweesweet@naver.com>
2023-12-03 23:46:28 +0900

    Time: 60 ms (52.54%), Space: 46.3 MB (5.73%) - LeetHub

--- Leetcode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
+++ Leetcode/Easy/0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -11,31 +11,23 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-  let arr=[]
-  function dfs(num,node){
-    if(node.left===null&&node.right===null){
-        arr.push(num)
-        return
-    }
-    if(node.left!==null){
-        dfs(num+1,node.left)
-
-    }
-    if(node.right!==null){
-           dfs(num+1,node.right)
-    }
-} 
   if(!root){
-    return 0
+      return 0
+  }  
+  let queue=[[root,1]]
+  while(queue.length){
+      let [r,depth]=queue.shift()
+      if(r.left&&r.right){
+          queue.push([r.left,depth+1],[r.right,depth+1])
+      }else if(r.right){
+          queue.push([r.right,depth+1])
+      }else if(r.left){
+          queue.push([r.left,depth+1])
+      }
+    if(!queue.length){
+        return depth
+    }    
+
   }
-  if(root.left!==null){
-      dfs(2,root.left)
-  }
-  if(root.right!==null){
-        dfs(2,root.right)
-  }
-  if(arr.length===0){
-    return 1
-  }
-    return Math.max(...arr)
+
 };
