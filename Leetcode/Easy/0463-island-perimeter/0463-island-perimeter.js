1731944d3b4ee6609b41bfbad5da06a489611d64
Yunjin Lee <sweesweet@naver.com>
2023-12-06 23:45:09 +0900

    Time: 144 ms (30.24%), Space: 50.8 MB (52.45%) - LeetHub

--- Leetcode/Easy/0463-island-perimeter/0463-island-perimeter.js
+++ Leetcode/Easy/0463-island-perimeter/0463-island-perimeter.js
@@ -4,23 +4,41 @@
  */
 var islandPerimeter = function(grid) {
     let sum=0
-    const dfs=(x,y)=>{
-        if(x<0||y<0||x>=grid.length||y>=grid[0].length||grid[x][y]===0){
-           return 1
-        }
-        if(grid[x][y]===-1){
-            return 0
-        }
-        grid[x][y]=-1
-        return dfs(x-1,y)+dfs(x,y-1)+dfs(x+1,y)+dfs(x,y+1)
-    }
+    let row=grid.length,col=grid[0].length
     for(let i=0;i<grid.length;i++){
         for(let j=0;j<grid[0].length;j++){
-            if(grid[i][j]){
-                sum+=dfs(i,j)
-                break
+            if(grid[i][j]===1){
+                grid[i][j]=-1
+                sum+=bfs(i,j)
             }
         }
     }
+    function bfs(q,w){
+        let peri=0
+        let queue= [[q,w]]
+        let direction=[[0,1],[0,-1],[1,0],[-1,0]]
+        while(queue.length){
+            let [i,j]=queue.shift()
+            for(let [x,y] of direction){
+                let left=x+i
+                let right=y+j
+                if(left>=0&&left<row&&right>=0&&right<col){
+                  if(grid[left][right]===1){
+                      grid[left][right]=-1
+                      queue.push([left,right])
+                  }else if(grid[left][right]===0){
+                      peri+=1
+                  }      
+                }else{
+                  peri+=1  
+                }
+
+            }
+
+        }
+
+        return peri
+
+    }  
     return sum
 };
