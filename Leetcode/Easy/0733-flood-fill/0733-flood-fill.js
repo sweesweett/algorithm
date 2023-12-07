58a1fda109f820364d09773b720acacd14d32eee
Yunjin Lee <sweesweet@naver.com>
2023-12-07 20:42:05 +0900

    Time: 72 ms (21.51%), Space: 45.3 MB (8.13%) - LeetHub

--- Leetcode/Easy/0733-flood-fill/0733-flood-fill.js
+++ Leetcode/Easy/0733-flood-fill/0733-flood-fill.js
@@ -10,20 +10,22 @@ var floodFill = function(image, sr, sc, color) {
     if(start===color){
         return image
     }
-  const dfs=(x,y)=>{
-      if(x<0||x>=image.length||y<0||y>=image[0].length){
-          return;
-      }
-      if(image[x][y]===start){
-          image[x][y]=color
-      }else{
-          return;
-      }
-      dfs(x-1,y)
-      dfs(x+1,y)
-      dfs(x,y-1)
-      dfs(x,y+1)
-  }
-    dfs(sr,sc)
-  return image  
+    let queue=[[sr,sc]]
+    let direction=[[0,1],[0,-1],[1,0],[-1,0]]
+    let row=image.length,col=image[0].length
+    image[sr][sc]=color
+    while(queue.length){
+        let [i,j]=queue.shift()
+        for(let [x,y] of direction){
+            let dx=i+x
+            let dy=j+y
+            if(dx>=0&&dx<row&&dy>=0&&dy<col){
+                if(image[dx][dy]===start){
+                    image[dx][dy]=color
+                    queue.push([dx,dy])
+                }
+            }
+        }
+    }
+    return image
 };
