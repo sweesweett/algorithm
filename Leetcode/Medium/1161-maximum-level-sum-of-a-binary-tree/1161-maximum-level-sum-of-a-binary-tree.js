100b97138c1269ab559ab52c56af8932d51ab4bc
Yunjin Lee <sweesweet@naver.com>
2023-12-19 21:32:33 +0900

    Time: 144 ms (19.83%), Space: 67.8 MB (81.16%) - LeetHub

--- Leetcode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ Leetcode/Medium/1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -1,39 +1,31 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
 var maxLevelSum = function(root) {
     let arr=[]
-    let max=-Infinity
-    let idx=-1
-    const dfs=(root,level)=>{
-        if(!root){
-            return;
-        }
-        if(arr[level]===undefined){
-            arr.push(root.val)
-        }else{
-            arr[level]+=root.val
-        }
-        dfs(root.left,level+1)
-        dfs(root.right,level+1)    
-    }
-    dfs(root,0)
+    const bfs=(root)=>{
+      let stack=[[root,0]]
+      while(stack.length){
+          let [node,depth]=stack.pop()
+          if(!node){
+              continue
+          }
+          if(arr[depth]===undefined){
+              arr[depth]=node.val
+          }else{
+              arr[depth]+=node.val
+          }
+          stack.push([node.left,depth+1],[node.right,depth+1])  
 
-    arr.forEach((el,i)=>{
-        if(max<el){
-            max=el
-            idx=i
-        }
-    })
-    return idx+1
-    
+      }
+
+  }
+  bfs(root)
+  let max=[-Infinity,0]
+  arr.forEach((el,i)=>{
+      if(el>max[0]){
+          max[0]=el
+          max[1]=i
+      }
+
+  })
+    return max[1]+1
 };
+
