aaf004ba496b103a97b6e297abb964de761b889e
Yunjin Lee <sweesweet@naver.com>
2023-12-21 20:52:05 +0900

    Time: 74 ms (28.35%), Space: 49 MB (18.11%) - LeetHub

--- Leetcode/Medium/1254-number-of-closed-islands/1254-number-of-closed-islands.js
+++ Leetcode/Medium/1254-number-of-closed-islands/1254-number-of-closed-islands.js
@@ -1,23 +1,36 @@
 var closedIsland = function(grid) {
-  let count=0
-  let [row,col]=[grid.length,grid[0].length]
-    const dfs=(x,y)=>{
-      if(x<0||y<0||x>=row||y>=col){
-         return false
-      }
-      if(grid[x][y]===1){
-         return true
-      }
-      grid[x][y]=1 
-      let [up,down,left,right]=[dfs(x-1,y),dfs(x+1,y),dfs(x,y-1),dfs(x,y+1)]
-      return up&&down&&left&&right
-    }
-    for(let i=0;i<row;i++){
-      for(let j=0;j<col;j++){
-        if(grid[i][j]===0&&dfs(i,j)){
-            count++
+    let count=0
+    for(let i=0;i<grid.length;i++){
+      for(let j=0;j<grid[0].length;j++){
+        if(grid[i][j]===0){
+            count+=bfs(i,j,grid)
         }
       }
     }
     return count
 };
+function bfs(i,j,grid){
+    let stack=[[i,j]]
+    let rows=grid.length,cols=grid[0].length
+    let directions= [[0,1],[1,0],[0,-1],[-1,0]]
+    let isTrue=true
+    grid[i][j]=1
+    while(stack.length){
+        let [q,w]=stack.pop()
+        for(let [dx,dy] of directions){
+            let x=dx+q
+            let y=dy+w
+            if(x<0||y<0||x>=rows||y>=cols){
+                isTrue=false
+                continue
+            }
+            if(grid[x][y]===0){
+                grid[x][y]=1
+                stack.push([x,y])
+            }
+
+        }
+    }
+    return isTrue?1:0
+}
+
