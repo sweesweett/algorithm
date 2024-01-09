ea228ed002393edbd53291888a582946e2f463b7
Yunjin Lee <sweesweet@naver.com>
2024-01-09 15:24:03 +0900

    Time: 123 ms (67.03%), Space: 96.6 MB (60.47%) - LeetHub

---
+++ Leetcode/Easy/0938-range-sum-of-bst/0938-range-sum-of-bst.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+var rangeSumBST = function(root, low, high) {
+    let sum=0
+    const dfs=(node)=>{
+        if(!node){
+            return
+        }
+        let val= node.val
+        if(val<=high&&val>=low){
+            sum+=node.val
+         
+        }
+        dfs(node.left)
+        dfs(node.right)
+     
+
+    }
+    dfs(root)
+    return sum
+};
