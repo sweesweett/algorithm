5ef433ea3592d2386efb9ad24ff7bff43dbdb594
Yunjin Lee <sweesweet@naver.com>
2024-08-12 22:38:37 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 14.85 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -0,0 +1,64 @@
+function solution(orders, course) {
+    let obj={}
+    let obj2={}
+    let maxVal
+    let ans=[]
+    for(let num of course){
+        obj2[num]=[]
+        maxVal=Math.max(num,maxVal)
+    }
+    for(let val of orders){
+      recursion(0,[],val)     
+    }
+    for(let key in obj2){
+        obj2[key].sort((a,b)=>obj[b]-obj[a])
+        let max;
+        for(let val of obj2[key]){
+            if(obj[val]===1)break
+            if(!max){
+                ans.push(val)
+                max=obj[val]
+                continue
+            }
+            if(max===obj[val]){
+                ans.push(val)
+            }else{
+                break
+            }
+        }
+    }
+    return ans.sort()
+    
+    
+    
+   function recursion(idx,arr,origin,set){
+       if(arr.length>=maxVal){
+        return
+    }
+    if(idx>=origin.length){
+        return
+    }
+    for(let i=idx;i<origin.length;i++){
+
+        let newArr=[...arr,origin[i]].sort()
+        let join=newArr.join('')
+        
+      if(newArr.length>=2){
+        if(obj2[newArr.length]){
+            if(!obj[join]){
+            obj[join]=0  
+            obj2[newArr.length].push(join)
+         }
+          obj[join]+=1   
+        }  
+        
+            
+      }
+
+        recursion(i+1,[...arr,origin[i]],origin)
+
+    }
+    
+    
+}
+}
