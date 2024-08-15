e7f58c9256a43853efe463062b89520296e09347
Yunjin Lee <sweesweet@naver.com>
2024-08-16 00:35:14 +0900

    [level 2] Title: 테이블 해시 함수, Time: 25.32 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.js
@@ -5,17 +5,6 @@ function solution(data, col, row_begin, row_end) {
        }
        return a[col-1]-b[col-1]
    })
-  let newData=data.map((arr,i)=>arr.reduce((a,b)=>a+b%(i+1),0))
-  let ans;
-  if(row_begin===row_end){
-      return newData[row_begin-1]
-  }  
-  for(let i=row_begin-1;i<row_end;i++){
-      if(ans===undefined){
-          ans=newData[i]
-          continue
-      }
-      ans=ans^newData[i]
-  }  
-    return ans
+  return data.map((arr,i)=>arr.reduce((a,b)=>a+b%(i+1),0))
+                  .slice(row_begin-1,row_end).reduce((a,b)=>a^b)  
 }
