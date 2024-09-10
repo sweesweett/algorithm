96cf0184c31c8eea504f324ce629f409d54523ba
Yunjin Lee <sweesweet@naver.com>
2024-09-11 00:03:30 +0900

    Time: 99 ms (53.44%), Space: 58.4 MB (50.26%) - LeetHub

--- Leetcode/Medium/2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
+++ Leetcode/Medium/2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.js
@@ -10,42 +10,34 @@
  * @return {ListNode}
  */
 var insertGreatestCommonDivisors = function(head) {
-    let list= head
-
-    while(list){
-       if(list.next){
-           let middleNode=new ListNode(gcd(list.val,list.next.val))
-           let tmp= list.next
-           list.next= middleNode
-           list=list.next
-           list.next= tmp
-       }
-       list=list.next
-
+    let node=head
+    let prev;
+    while(node.next){
+        let val =node.next.val
+        let next= new ListNode(gcd([node.val,val]))
+        let tmp=node.next
+        node.next=next
+        next.next=tmp
+        node=tmp
     }
- return head
-    
-    
-    
-    
+   return head
 };
-function gcd(val1,val2){
-        if(val1===val2){
-            return val1
-        }
-        let p,q;
-        if(val1<=val2){
-            [p,q]=[val1,val2]
-        }else{
-            [p,q]=[val2,val1]
-        }
-        while(true){
-        if(q%p===0){
+function gcd(val){
+    let [p,q]=val
+    if(p===q){
+        return p
+    }
+    if(p>q){
+        [p,q]=[q,p]
+    }
+    while(q>1){
+        let tmp=q%p
+        if(tmp===0){
             break
         }
-        tmp=q%p
         q=p
         p=tmp
     }
+
     return p
-    }
+}
