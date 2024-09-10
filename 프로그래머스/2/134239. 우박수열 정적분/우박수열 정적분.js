0e636ac69c7b86c1fd9777f69429d541fb419683
Yunjin Lee <sweesweet@naver.com>
2024-09-10 22:13:03 +0900

    [level 2] Title: 우박수열 정적분, Time: 43.44 ms, Memory: 40.8 MB -BaekjoonHub

--- 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.js
+++ 프로그래머스/2/134239. 우박수열 정적분/우박수열 정적분.js
@@ -1,31 +1,6 @@
 function solution(k, ranges) {
-    let num=k
-    let i=0
-    let arr=[]
-    let ans=[]
-    arr.push([i,num])
-    i++
-    while(num!==1){
-        if(num%2){
-            num=num*3+1
-
-        }else{
-            num/=2
-        }
-        arr.push([i,num])
-        i++
-    }
-    ans.push(0)
-    for(let i=1;i<arr.length;i++){
-        let [x1,y1]=arr[i-1]
-        let [x2,y2]=arr[i]
-        let integral=(y2-y1)/(x2-x1)/2
-        let con=-integral*2*x1+y1
-        ans.push((ans.at(-1)||0)+(integral*(x2**2)+con*x2)-(integral*(x1**2)+con*x1))
-
-        
-        
-    }
+    let arr=makingCoordArr(k)
+    let reduceArr=makingIntegralArr(arr)
     let answer=[]
     for(let [start,end] of ranges){
         let fixedEnd=arr.length-1+end
@@ -37,13 +12,42 @@ function solution(k, ranges) {
             answer.push(-1)
             continue
         }
-        let endVal=ans[fixedEnd]
-        let startVal=ans[start]
+        let endVal=reduceArr[fixedEnd]
+        let startVal=reduceArr[start]
         if(!endVal===undefined||!startVal===undefined){
             answer.push(-1)
         }else{
-            answer.push(Math.abs(endVal-startVal))
+            answer.push(endVal-startVal)
         }
     }
     return answer
+}
+
+function makingIntegralArr(arr){
+    let reduceArr=[0]
+    for(let i=1;i<arr.length;i++){
+        let [x1,y1]=arr[i-1]
+        let [x2,y2]=arr[i]
+        let integral=(y2-y1)/(x2-x1)/2
+        let con=-integral*2*x1+y1
+        reduceArr.push(reduceArr.at(-1)+integral*(x2**2-x1**2)+con*(x2-x1))
+    }
+    return reduceArr
+}
+
+function makingCoordArr(num){
+    let i=1
+    let arr=[[0,num]]
+    
+    while(num!==1){
+        if(num%2){
+            num=num*3+1
+
+        }else{
+            num/=2
+        }
+        arr.push([i,num])
+        i++
+    }
+    return arr
 }
